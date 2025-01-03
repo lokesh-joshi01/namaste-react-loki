04996717ecffba61fae7a1f014f953340aa658d2
Lokesh <cricketjoshi01@gmail.com>
2025-01-03 14:57:18 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,21 @@
+const parent = React.createElement(
+    "div",
+    {id: "parent"},
+    [React.createElement(
+        "div",
+        {id:"child"},
+    [React.createElement("h1",{},"I am H1"),React.createElement("h2",{},"I am H2")]),
+             React.createElement("div",
+            {id:"child2"},
+        [React.createElement("h1",{},"I am H1 from c2"),React.createElement("h2",{},"I am H2")])
+    ])
+
+// const heading = React.createElement(
+
+//     "h1",
+//     {id: "heading"},"Hello from React"
+// );
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById("root")
+);
+root.render(parent);
