cf2a85f9db40d3fd10d181b19c082c02ab66403b
Lokesh <cricketjoshi01@gmail.com>
2025-01-04 16:15:36 +0530

    Done till res card

--- App.js
+++ App.js
@@ -1,24 +1,72 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement(
-    "div",
-    {id: "parent"},
-    [React.createElement(
-        "div",
-        {id:"child"},
-    [React.createElement("h1",{},"I am H1"),React.createElement("h2",{},"I am H2")]),
-             React.createElement("div",
-            {id:"child2"},
-        [React.createElement("h1",{},"I am H1 from c2 hit"),React.createElement("h2",{},"I am H2")])
-    ])
+const Header =() =>{
+    return(
+        <div className="header">
+            <div className="logo-container">
+               <img className="logo" src="https://marketplace.canva.com/EAFpeiTrl4c/1/0/1600w/canva-abstract-chef-cooking-restaurant-free-logo-9Gfim1S8fHg.jpg" />
+            </div>
+            <div className="nav-items">
+            <ul>
+                <li>Home</li>
+                <li>About Us</li>
+                <li>Contact Us</li>
+                <li>Cart</li>
+            </ul>
+            </div>
+        </div>
+    )
+}
 
-// const heading = React.createElement(
+const ResCard =() =>{
+    return(
+        <div className="res-card">
+            <img className="res-logo" alt="res-logo" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/972a37599772cdc7df93a0855ad87591"/>
+            <h3>Maggi Point </h3>
+            <h6>North Indian, Chinese</h6>
+            <h5>4.5 stars</h5>
+            <h5>500 for two</h5>
+        </div>
+    )
+}
 
-//     "h1",
-//     {id: "heading"},"Hello from React"
-// );
-console.log(parent);
-const root = ReactDOM.createRoot(document.getElementById("root")
-);
-root.render(parent);
+const Body = ()=>{
+    return(
+        <div className="body">
+            <div className="search">
+                Search Bar
+                <input className="search-input" type="text" placeholder="search here"></input>
+            </div>
+            <div className="res-container">
+                <ResCard/>
+                <ResCard/>
+                <ResCard/>
+                <ResCard/>
+                <ResCard/>
+                <ResCard/>
+                <ResCard/>
+                <ResCard/>
+                <ResCard/>
+                <ResCard/>
+                <ResCard/>
+                <ResCard/>
+                <ResCard/>
+                <ResCard/>
+                <ResCard/>
+            </div>
+        </div>
+    )
+}
+
+const AppLayout = () =>{
+    return (
+        <div className="app">
+        <Header/>
+        <Body />
+        </div>
+    )
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout/>);
